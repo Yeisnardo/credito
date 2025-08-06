db7252567deaed86fe8d2bf930f4bd48f4b54428
Jose Colmenares <josefe1309@gmail.com>
2025-08-06 18:49:38 -0400

    index src

--- backend/config/conexion.js
+++ backend/config/conexion.js
@@ -4,11 +4,13 @@ const pool = new Pool({
   user: 'postgres',        // reemplaza con tus datos
   host: 'localhost',
   database: 'siccee', // reemplaza con tu base
-  password: 'Yeisnardo06',   // reemplaza con tu password
+  password: '13092003',   // reemplaza con tu password
   port: 5432,
 });
 
 const query = (text, params) => pool.query(text, params);
+
+
 
 module.exports = { query };
 
