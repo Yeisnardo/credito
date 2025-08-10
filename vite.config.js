f1fd919afd44485d615067485a5617ab75e443f4
Jose Colmenares <josefe1309@gmail.com>
2025-08-10 17:26:50 -0400

    cansancio

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',  // Cambia a '/ruta/' si lo despliegas en un subdirectorio
 })
