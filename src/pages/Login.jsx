c7a0d8b4ee508b84a7ce6da6224c21c84942972f
Jose Colmenares <josefe1309@gmail.com>
2025-09-17 11:15:36 -0400

    feo x22

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -59,7 +59,7 @@ const Login = ({ setUser }) => {
         icon: "success",
         title: "¡Bienvenido!",
         text: response.data.message || "Inicio de sesión exitoso",
-        timer: 1500,
+        timer: 8500,
         showConfirmButton: false,
       }).then(() => {
         navigate("/dashboard");
