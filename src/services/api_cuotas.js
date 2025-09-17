a8bf2cc46db59cb37c94c039e821b7c81d2f8c2e
Yeisnardo <Yeisnardo30@gmail.com>
2025-09-17 11:11:43 -0400

    r

--- src/services/api_cuotas.js
+++ src/services/api_cuotas.js
@@ -6,18 +6,12 @@ const api = axios.create({
 
 // Funciones API para cuotas
 
-export const getCuotas = async () => {
-  const response = await api.get('/api/cuotas');
-  return response.data;
-};
-
-export const getContratoPorCedula = async (id_contrato) => {
-  const response = await api.get(`/api/cuotas/${id_contrato}`);
+export const getContratoPorId = async (cedula_emprendedor) => {
+  const response = await api.get(`/api/cuotas/${cedula_emprendedor}`);
   return response.data;
 };
 
 
 export default {
-  getCuotas,
-  getContratoPorCedula
+  getContratoPorId
 };
